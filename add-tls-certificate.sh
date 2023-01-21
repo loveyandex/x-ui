6f855f36ec449546a0f059101c0daa0530110527
loveyandex <53381292+loveyandex@users.noreply.github.com>
2023-01-21 11:09:07 +0330

    Update add-tls-certificate.sh

--- add-tls-certificate.sh
+++ add-tls-certificate.sh
@@ -7,7 +7,7 @@ apt update -y && apt upgrade -y
 
 apt-get install certbot -y
 
-domain="cccc.com"
+domain="instance-1.crmocean.ir"
 
 certbot certonly --standalone -d $domain --register-unsafely-without-email --non-interactive --agree-tos
 
