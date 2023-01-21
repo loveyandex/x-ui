351a97b6bb084aa162802aaddf2bc8594417b703
loveyandex <53381292+loveyandex@users.noreply.github.com>
2023-01-21 12:50:50 +0330

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -3,4 +3,4 @@
 host=$1
 echo $host
 
-bash <(curl -Ls https://raw.githubusercontent.com/loveyandex/x-ui/master/add-tls-certificate.sh) "$host"
+bash <(curl -Ls https://raw.githubusercontent.com/loveyandex/x-ui/master/add-tls-certificate.sh) $host
